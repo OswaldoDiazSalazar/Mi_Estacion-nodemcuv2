6acf7f0727a721831dd10a3cd02bf9adafbfbbee
Oswaldo Diaz <diazou@gmail.com>
2018-10-04 22:47:02 +0100

    formateando la salida a texto tamano definido

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Mi_Estacion
+Programa cargado en un ESP8266 NodeMCU v2
+Toma los datos de Temperatura y Humedad del DHT11
+Toma la luminosidad por el puerto analogico con un sensor analogico 
+Toma los datos de temperatura, presion Atmosferica y Altitud con ref nivel del mar Lisboa.
+
+La conexion con el telefono android es via wifi utiizando el router de la casa.
+la aplicacion cargada en el telefono fue hecha con MIT Invetors 2
+Anexo los archivos (leds.aia).
--- src/ESP_Servidor_leds.ino
+++ src/ESP_Servidor_leds.ino
@@ -8,7 +8,15 @@ SimpleDHT11 dht11;
 Adafruit_BMP085 bmp;
 const char ssid[] = "NOS-9D9E";
 const char pass[] = "5VNL9AY6";
-
+byte temperatura = 0;
+byte humedad = 0;
+float luminosidad = 0;
+double T,P,A;
+int val = 0;
+char resultL[8];
+char resultT[8];
+char resultP[8];
+char resultA[8]; 
 // Create an instance of the server
 // specify the port to listen on as an argument
 WiFiServer server(80);
@@ -18,8 +26,9 @@ void setup() {
   delay(10);
   pinMode(A0, INPUT);
   // prepare GPIO2
-  pinMode(16, OUTPUT);
-  digitalWrite(16, 0);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, 0);
+  // preparando el sensor bmp180
   Wire.begin(0, 2);
   if(!bmp.begin()){
     Serial.println("BMP init failed!");
@@ -51,19 +60,100 @@ void setup() {
 }
 
 void loop() {
-  byte temperatura = 0;
-  byte humedad = 0;
-  float luminosidad = analogRead(A0);
+  
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  //Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  //Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  
+  if (req.indexOf("/gpio/0") != -1){ 
+    val = 0;
+    // Set GPIO2 according to the request
+    digitalWrite(4, val);
+    leer_sensores();
+    //respuesta();
+    }
+  else if (req.indexOf("/gpio/1") != -1){ 
+    val = 1;
+    // Set GPIO2 according to the request
+    digitalWrite(4, val);
+    leer_sensores();
+    //respuesta();
+    }
+  else if (req.indexOf("/gpio/A") != -1){
+    leer_sensores();
+    //respuesta();
+    }
+ //else {
+    //Serial.println("invalid request");
+ //   client.stop();
+ //   return;
+ // }
+// Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+  s += (val)?"OFF":"_ON";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.flush();
+  client.print(s);
+  client.print("<P>");
+  client.print("Temperatura: ");
+  client.print(temperatura);
+  //client.print("<P>");
+  client.print(" Humedad: ");
+  client.print(humedad);
+  //client.print("<P>");
+  client.print(" Luminosidad: ");
+  client.print(resultL);
+  client.print(" Temperatura2: ");
+  client.print(resultT);
+  client.print(" Presion Atmosferica: ");
+  client.print(resultP);
+  client.print(" Altura: ");
+  client.print(resultA);
+  delay(10);
+  //Serial.println("Client disonnected");
+ // client.flush();
+ 
+ // client.stop();
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+}
+void leer_sensores() {
+  // procedimiento para leer los sensores
+  luminosidad = analogRead(A0);
+  //luminosidad = 100 - luminosidad;
   delay(300);
   if (dht11.read(pinDHT11, &temperatura, &humedad, NULL)) {
     Serial.print("Read DHT11 failed.");
     return;
   }
-double T,P,A;
+
   T = bmp.readTemperature();
   P = bmp.readPressure()/100;
   A = bmp.readAltitude(P0)-10;
  
+
+  dtostrf(luminosidad, 6, 2, resultL);
+  dtostrf(T, 6, 2, resultT);
+  dtostrf(P, 6, 2, resultP);
+  dtostrf(A, 6, 2, resultA);
   //    if(T !=0 )  {
   //      Serial.print("T = \t");Serial.print(T,2); Serial.print(" degC\t");
   //      Serial.print("P = \t");Serial.print(P,2); Serial.print(" mBar\t");
@@ -74,68 +164,9 @@ double T,P,A;
   //      Serial.println("Error.");
   //
   //    }
-  
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
-  
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-  
-  // Match the request
-  int val;
-  if (req.indexOf("/gpio/0") != -1)
-    val = 0;
-  else if (req.indexOf("/gpio/1") != -1)
-    val = 1;
-  else {
-    Serial.println("invalid request");
-    client.stop();
-    return;
-  }
-
-  // Set GPIO2 according to the request
-  digitalWrite(16, val);
-  
-  client.flush();
-
-  // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
-  s += (val)?"_ON":"OFF";
-  s += "</html>\n";
-
-  // Send the response to the client
-  client.print(s);
-  client.print("<P>");
-  client.print("Temperatura: ");
-  client.print(temperatura);
-  //client.print("<P>");
-  client.print(" Humedad: ");
-  client.print(humedad);
-  //client.print("<P>");
-  client.print(" Luminosidad: ");
-  client.print(luminosidad);
-  client.print(" Temperatura2: ");
-  client.print(T);
-  client.print(" Presion Atmosferica: ");
-  client.print(P);
-  client.print(" Altura: ");
-  client.print(A);
-  delay(10);
-  Serial.println("Client disonnected");
-  client.flush();
-  //client.stop();
-
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed
 }
+
+//void respuesta() {
+// Rutina para dar respuesta al cliente
+
+//}
